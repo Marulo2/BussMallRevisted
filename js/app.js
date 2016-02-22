b52bc1deb04d222f14b87081a093a8332ca0a4fd
Mars <marshallmiller92@gmail.com>
2016-02-22 15:11:00 -0800

    setting up event handlers and functions

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 //need to flowchart before anything else
+
+var one = document.getElementById('1');
+var two = document.getElementById('2');
+var three = document.getElementById('3');
+
+one.addEventListener('click', oneHandle);
+two.addEventListener('click', twoHandle);
+three.addEventListener('click', threeHandle);
+
+function oneHandle() {
+  console.log('clicked!');
+}
