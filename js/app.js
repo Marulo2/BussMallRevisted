a5e9f1cc18cc546394146fd251285d7bbe69620b
Mars <marshallmiller92@gmail.com>
2016-02-25 12:04:26 -0800

    built constructor function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 //need to flowchart before anything else
 
+function products(name, path) {
+  this.name = name;
+  this.path = path;
+  this.clicks = 0;
+}
+
+var allProducts = [new products('bag', 'img/bag.jpg'),
+                    new products('banana', 'img/banana.jpg'),
+                    new products('boots', 'img/banana.jpg'),
+                    new products('chair', 'img/chair.jpg'),
+                    new products('cthulu', 'img/cthulu.jpg')]
+
+
+
 var one = document.getElementById('1');
 var two = document.getElementById('2');
 var three = document.getElementById('3');
